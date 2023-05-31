66e109676812188e29581f6175e596cd2975b936
Sanmitha <sanmithasadanand@gmail.com>
2023-05-31 20:07:11 +0530

    Specify parameters for registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,19 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js"
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    try {
+      const {
+        firstName,
+        lastName,
+        email,
+        password,
+        picturePath,
+        friends,
+        location,
+        occupation,
+      } = req.body;
+    }
+}
