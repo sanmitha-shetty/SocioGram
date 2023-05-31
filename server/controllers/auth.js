6a96242a8beac78b0f4fa46f970ef63a10cb5445
Sanmitha <sanmithasadanand@gmail.com>
2023-05-31 20:30:13 +0530

    Add assets

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -15,12 +15,25 @@ export const register = async (req, res) => {
         location,
         occupation,
       } = req.body;
-      
+
       const salt = await bcrypt.genSalt();
       const passwordHash = await bcrypt.hash(password, salt);
 
-    }
-    catch (err) {
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: passwordHash,
+        picturePath,
+        friends,
+        location,
+        occupation,
+        viewedProfile: Math.floor(Math.random() * 10000),
+        impressions: Math.floor(Math.random() * 10000),
+      });
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
         res.status(500).json({ error: err.message });
-      }
+     }
 };
