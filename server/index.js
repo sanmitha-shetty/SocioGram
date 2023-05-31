dc056d8fa5376f0cc57bde11bb54b746c3412442
Sanmitha <sanmithasadanand@gmail.com>
2023-05-31 19:56:51 +0530

    Create controllers for authentication

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,10 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* Routes */
+app.post("/auth/register", upload.single("picture"), register);
+
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001
