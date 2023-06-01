8bf5ec2b277e6746005718b0d10d326b4e309e0e
Sanmitha <sanmithasadanand@gmail.com>
2023-06-01 15:21:23 +0530

    Add posts routes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import {register} from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +42,13 @@ const upload = multer({ storage });
 
 /* Routes with files*/
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 
 /* Routes */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001
