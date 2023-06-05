2c5d007eb556702a4d227b54d5f4e932d67a95e4
Sanmitha <sanmithasadanand@gmail.com>
2023-06-06 01:38:42 +0530

    Fix setup errors

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,20 @@ import { themeSettings } from "./theme";
 
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-      <Routes>
-          <Route path ="/" element ={<LoginPage/>} />
-          <Route path ="/home" element ={<HomePage/>} />
-          <Route path ="/profile/:userId" element ={<ProfilePage/>} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path ="/" element ={<LoginPage/>} />
+            <Route path ="/home" element ={<HomePage/>} />
+            <Route path ="/profile/:userId" element ={<ProfilePage/>} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
