d687e6ec1a5e7fbfba8e14d85c20c691cda172bb
Sanmitha <sanmithasadanand@gmail.com>
2023-06-06 01:24:47 +0530

    Setup themees and color

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+      <Routes>
+          <Route path ="/" element ={<LoginPage/>} />
+          <Route path ="/home" element ={<HomePage/>} />
+          <Route path ="/profile/:userId" element ={<ProfilePage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
