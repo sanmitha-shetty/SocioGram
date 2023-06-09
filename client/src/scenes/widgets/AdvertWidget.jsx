26875d5a01e75dab0c6ad1a49ca16a574da65b3e
Sanmitha <sanmithasadanand@gmail.com>
2023-06-09 22:47:57 +0530

    Modify advertisement

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -24,8 +24,8 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>SugarCosmetics</Typography>
+        <Typography color={medium}>sugarcosmetics.com</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
         Your pathway to stunning and immaculate beauty and made sure your skin
